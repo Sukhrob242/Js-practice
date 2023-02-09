1bf915fa3664ec504d1961b0ddb121de656829c4
Sukhrobjon <Sukhrob.toshtemirov@inbox.ru>
2023-02-09 21:51:25 +0500

    add code to js file

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 
 
-const numberOfSeries = +prompt("what is your favourite movie")
+const numberOfSeries = +prompt("how many series or movies have you watched? ?" , "") ;
+
+const seriesDB = {
+    count: numberOfSeries,
+    serial: {},
+    actors:{},
+    genres:[],
+    private:false,
+} 
+
+const  question = prompt("your last movie?")
+
